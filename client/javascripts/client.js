c18caaa75f57269727770f2c9329a6721b11df2c
vtno <lynx.tno@gmail.com>
2016-03-22 22:37:46 +0700

    Establish connection to server.

--- client/javascripts/client.js
+++ client/javascripts/client.js
@@ -2,49 +2,29 @@ var socket = io.connect('http://localhost:9999/')
 socket.on('connect',()=>{
   console.log('socket connected!')
 })
-socket.emit('hello', {text: "yeaahhhh"})
 Template.home.events({
     "submit .ip-form": (event) => {
       // Prevent default browser form submit
       event.preventDefault()
 
       // Get value from form element
-      var ip = event.target.ip.value;
-      var port = event.target.port.value;
-      console.log(ip)
-      console.log(port)
+      var username = event.target.user.value
 
+      console.log('username='+username)
 
-      //Sent value to wait opponent page
-      RoutingInfo.insert({
-        ip: ip,
-        port: port,
-        createAt: new Date()
-      })
-      console.log('insert success')
-      console.log('ip='+ip)
-      console.log('port='+port)
+      socket.emit('playerName',{user: username})
       Router.go('/waitOpponent')
       // Clear form
-      event.target.ip.value = ""
-      event.target.port.value = ""
+      event.target.user.value = ""
 
     }
 });
-Template.waitOpponent.helpers({
-  routingData: ()=>{
-    // return RoutingInfo.find({})
-    console.log(RoutingInfo.findOne({}, {sort: {createAt: -1, limit: 1}}))
-    return RoutingInfo.findOne({}, {sort: {createAt: -1, limit: 1}})
-  }
-})
 
-Template.main.onRendered(()=>{
-  $(document).ready(()=>{
-    console.log("render main")
-    let script = document.createElement('script')
-    script.type = "text/javascript"
-    script.src = "socket.io/socket.io.js"
-    // $('#script_div').append(script)
+Template.waitOpponent.onRendered(()=>{
+  console.log('enter waitopp')
+  socket.emit("getPlayerData")
+  socket.on("playerData",(name)=>{
+      $('.name').append("Welcome "+name.user)
   })
+
 })
--- server/server.js
+++ server/server.js
@@ -1,13 +1,24 @@
 Meteor.startup(()=>{
   let server = Meteor.npmRequire('http').createServer()
   let io = Meteor.npmRequire('socket.io')(server)
+  let players ={}
+  let username = ""
   io.on('connection', (socket)=>{
     socket.on('hello',(data)=>{
       console.log('HELLO BISH')
       console.log(data.text)
     })
+    socket.on('playerName',(data)=>{
+      console.log(data)
+      username = data.user
+    })
+    socket.on('getPlayerData',()=>{
+      socket.emit('playerData',{
+        user:username
+      })
+    })
   })
   server.listen(9999,()=>{
-    console.log('listening on port 9999')
+    console.log('listening on port '+9999)
   })
 })
